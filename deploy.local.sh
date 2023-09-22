fdde361de7b09fa74e463ef0af5fc7d60efaa86e
Peter Maczo <peter.maczo@gmail.com>
2023-09-22 23:55:38 +0200

    fix: local deployment

--- deploy.local.sh
+++ deploy.local.sh
@@ -3,18 +3,16 @@ if [ "$1" != "dev" ] && [ "$1" != "prod" ]; then
   exit 1
 fi
 
-if [ -z "${S3_BUCKET}" ]; then
-  echo "ERR: S3_BUCKET is not provided"
-  exit 1
-fi
+source .env.$1
 
-if [ -z "${CLF_DIST}" ]; then
-  echo "ERR: CLF_DIST is not provided"
-  exit 1
+PROFILE=$2 
+if [ -z "${VAR}" ]; then
+  PROFILE="default"
 fi
 
 echo "----- S3 bucket: $S3_BUCKET"
 echo "----- CLF_DIST: $CLF_DIST"
+echo "----- PROFILE: $PROFILE"
 
 if [ ! -d "./dist" ]; then
   echo "ERR: dist directory not found"
@@ -24,10 +22,10 @@ fi
 cd dist
 
 echo "----- Deploying to S3 bucket: $S3_BUCKET"
-aws s3 sync . s3://$S3_BUCKET/ 
+aws s3 sync . s3://$S3_BUCKET/ --profile $PROFILE
 
 echo "----- Setting cache-control to 0"
-aws s3 cp index.html s3://$S3_BUCKET/index.html --cache-control max-age=0 
+aws s3 cp index.html s3://$S3_BUCKET/index.html --cache-control max-age=0 --profile $PROFILE
 
 echo "----- Invalidating CloudFront cache"
-aws cloudfront create-invalidation --distribution-id $CLF_DIST --paths '/index.html' 
+aws cloudfront create-invalidation --distribution-id $CLF_DIST --paths '/index.html' --profile $PROFILE
