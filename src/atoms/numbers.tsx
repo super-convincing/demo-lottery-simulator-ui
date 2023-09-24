9618061d3093784bc8e6c2f275f9e6e2f3b98beb
Peter Maczo <peter.maczo@gmail.com>
2023-09-24 22:58:42 +0200

    feat: refactor components

--- src/atoms/numbers.tsx
+++ src/atoms/numbers.tsx
@@ -1,12 +1,3 @@
-import { CNumber } from './fields'
-
-export const CWon = ({ nrMatches, nrWon }: { nrMatches: number, nrWon: number }) => {
-  return <div className='comp-won'>
-    <span className='nr-matches'>{nrMatches} matches</span>
-    <span className='nr-won'><CNumber value={nrWon} /></span>
-  </div>
-}
-
 export const CLotteryNumbers = ({ numbers, text }: { numbers: number[], text: string }) => {
   return <div className='comp-lottery-numbers'>
     <div className='text'>{text}</div>
