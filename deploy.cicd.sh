65bd456e68e34d83736341df70ebe32d070d33a5
Peter Maczo <peter.maczo@gmail.com>
2023-11-01 23:59:24 +0100

    conf: deploy.cicd.sh

--- deploy.cicd.sh
+++ deploy.cicd.sh
@@ -24,7 +24,6 @@ if [ -z "${CLF_DIST}" ]; then
   exit 1
 fi
 
-echo "----- PROFILE: $PROFILE"
 echo "----- S3 bucket: $S3_BUCKET"
 echo "----- S3 folder: $S3_FOLDER"
 echo "----- CLF_DIST: $CLF_DIST"
@@ -43,10 +42,10 @@ cd dist
 
 # -------- DEPLOY --------
 echo "----- Deploying to S3 bucket: $S3_BUCKET"
-aws s3 sync . s3://$S3_BUCKET/$S3_FOLDER/ --profile $PROFILE
+aws s3 sync . s3://$S3_BUCKET/$S3_FOLDER/
 
 echo "----- Setting cache-control to 0"
-aws s3 cp index.html s3://$S3_BUCKET/$S3_FOLDER/index.html --cache-control max-age=0 --profile $PROFILE
+aws s3 cp index.html s3://$S3_BUCKET/$S3_FOLDER/index.html --cache-control max-age=0
 
 echo "----- Invalidating CloudFront cache"
-aws cloudfront create-invalidation --distribution-id $CLF_DIST --paths /$S3_FOLDER/index.html --profile $PROFILE
+aws cloudfront create-invalidation --distribution-id $CLF_DIST --paths /$S3_FOLDER/index.html
